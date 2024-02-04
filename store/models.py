fee9f29f850dc45796fc29cb3a7751e38cd63c72
kavindus <kavindusachinthe@outlook.com>
2024-02-04 21:09:20 +0530

    models.py values updated. add new data models Product, Order, Customer

--- store/models.py
+++ store/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'C'
+    PAYMENT_STATUS = [(PAYMENT_STATUS_PENDING, 'Pending'),
+                      (PAYMENT_STATUS_COMPLETE, 'Complete'),
+                      (PAYMENT_STATUS_FAILED, 'Failed'),
+                      ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+
+    payment_status = models.CharField(choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING, max_length=1)
+
+
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.BigIntegerField(max_length=10)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)

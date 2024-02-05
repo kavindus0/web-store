09a5bd6ca3393d5bbff978e2e2f53f9ca46a7c17
kavindus <kavindusachinthe@outlook.com>
2024-02-05 13:41:01 +0530

    add new slugFeild

--- store/models.py
+++ store/models.py
@@ -26,6 +26,7 @@ class Customer(models.Model):
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
